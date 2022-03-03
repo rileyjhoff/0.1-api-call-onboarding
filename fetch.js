e5ced4a373e7ca0c03883c0ed33130aedafadef5
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-03 14:17:02 -0800

    added fetch fuction

---
+++ fetch.js
@@ -0,0 +1,8 @@
+export async function getPeople() {
+    let url = https://swapi.dev/api/people;
+    const resp = await fetch(url);
+    const json = await resp.json();
+    return json.results;
+}
+
+// To Do: write species function
