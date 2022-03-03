ff5da722e9ab61fe3c6691bfb433c4f660b2e3a5
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-03 15:26:06 -0800

    added functions for star wars species & added an event listener that will render people or species depending on the option selected

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions
-import { getPeople } from './fetch.js';
+import { getPeople, getSpecies } from './fetch.js';
 
 // console.log(getPeople);
 
@@ -11,26 +11,61 @@ const list = document.querySelector('#list');
 // console.log(template, selectEl, list);
 
 async function loadPeople() {
-    const people = await getPeople();
+    const peopleIndex = await getPeople();
 
-    list.classList.add('individuals');
+    list.classList.add('people');
 
-    for (let individuals of people) {
+    for (let people of peopleIndex) {
         const clone = template.content.cloneNode(true);
 
         const name = clone.querySelector('h2');
         const birth_year = clone.querySelector('h6');
         const mass = clone.querySelector('h5');
         
-        name.textContent = 'Name: ' + individuals.name;
-        birth_year.textContent = 'Birth Year: ' + individuals.birth_year;
-        mass.textContent = 'Weight: ' + individuals.mass;
+        name.textContent = 'Name: ' + people.name;
+        birth_year.textContent = 'Birth Year: ' + people.birth_year;
+        mass.textContent = 'Weight: ' + people.mass;
+
+        list.appendChild(clone);
+    }
+}
+
+async function loadSpecies() {
+    const speciesIndex = await getSpecies();
+
+    list.classList.add('species');
+
+    for (let species of speciesIndex) {
+        const clone = template.content.cloneNode(true);
+
+        const name = clone.querySelector('h2');
+        const average_lifespan = clone.querySelector('h6');
+        const classification = clone.querySelector('h5');
+        
+        name.textContent = 'Name: ' + species.name;
+        average_lifespan.textContent = 'Average Lifespan: ' + species.average_lifespan;
+        classification.textContent = 'Classification: ' + species.classification;
 
         list.appendChild(clone);
     }
 }
 
 // set event listeners 
+
+selectEl.addEventListener('change', async(e) => {
+    const selected = e.target.value;
+
+    if (selected === 'people') {
+        list.classList.remove('species');
+        list.innerHTML = '';
+        await loadPeople();
+    } else if (selected === 'species') {
+        list.classList.remove('people');
+        list.innerHTML = '';
+        await loadSpecies();
+    }
+});
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
--- fetch.js
+++ fetch.js
@@ -5,4 +5,11 @@ export async function getPeople() {
     return json.results;
 }
 
+export async function getSpecies() {
+    let url = 'https://swapi.dev/api/species';
+    const resp = await fetch(url);
+    const json = await resp.json();
+    return json.results;
+}
+
 // To Do: write species function
